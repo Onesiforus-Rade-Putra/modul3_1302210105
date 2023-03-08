4fb27a793a093659224cf879c0ea81fbe66aef10
Onesiforus-Rade-Putra <127029013+Onesiforus-Rade-Putra@users.noreply.github.com>
2023-03-08 16:50:11 +0700

    hasil ahkir

---
+++ modul3_1302210105/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modul3_1302210105
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        string y = "";
+        int angka1 = 0;
+        int angka2 = 0;
+        int keluar = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "8";
+                y += "8";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "1";
+                y += "1";
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "2";
+                y += "2";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "3";
+                y += "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "4";
+                y += "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "5";
+                y += "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "6";
+                y += "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "7";
+                y+= "7";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "9";
+                y += "9";
+            }
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            angka1 = int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void hasil_Click(object sender, EventArgs e)
+        {
+            angka2 = int.Parse(textBox1.Text);
+            keluar = angka2 + angka1;
+            textBox1.Text = keluar.ToString();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+                y += "0";
+            }
+        }
+    }
+}
